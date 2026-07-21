a5bc6aaaa8d9b6057e4c5d27d2f279c42cd23222
Ashif Eqbal <ashif8984@gmail.com>
2026-07-21 18:46:42 +0530

    updaed app.[py

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
     return jsonify({
         'hostname': socket.gethostname(),
         'timestamp': datetime.datetime.now(),
-        'message': 'Hello from Ashif app running and Docker and kates'
+        'message': 'Hello! from Ashif app running and Docker and kates'
         })
 
 @app.route('/api/v1/healthz')
